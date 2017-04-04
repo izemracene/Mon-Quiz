ebce3edccc0557f6b947fa6af42972e3940d66f4
izemracene <izemracenekhelili@gmail.com>
2017-04-04 23:05:40 +0200

    ajout dossier quiz

---
+++ quizz/quizz.php
@@ -0,0 +1,34 @@
+<DOCTYPE html>
+<html>
+<head>
+    <title>Mon Quizz</title>
+    <meta charset="UTF-8" />
+</head>
+
+<body>
+
+    <section>    
+        <article> 
+            <form action="reponses.php" method="POST" >   
+            <fieldset> 
+                <ul>
+                    <li><input type="radio" name= "q1" value="r1"/><label> réponse n°1 : r1 </label> </li>
+                    <li><input type="radio" name= "q1" value="r2"/><label> réponse n°2 : r2 </label> </li>
+                    <li><input type="radio" name= "q1" value="r3"/><label> réponse n°3 : r3 </label> </li>
+                </ul>
+
+                <ul>
+                    <li><input type="radio" name= "q2" value="r1"/><label> réponse n°1 : r1 </label> </li>
+                    <li><input type="radio" name= "q2" value="r2"/><label> réponse n°2 : r2 </label> </li>
+                    <li><input type="radio" name= "q2" value="r3"/><label> réponse n°3 : r3 </label> </li>
+                </ul>
+
+                <p><input type="submit" name="valider" value="Valider le Quizz"></p>
+            </fieldset> 
+            </form>    
+        </article>
+    </section>
+</body>
+</html>
+
+
---
+++ quizz/reponses.php
@@ -0,0 +1,13 @@
+<DOCTYPE html>
+<html>
+<head>
+    <title>Mon Quizz</title>
+    <meta charset="UTF-8" />
+</head>
+
+<body>
+<?php
+    echo '<pre>'; print_r($_POST);echo '</pre>'; 
+?>
+</body>
+</html>
